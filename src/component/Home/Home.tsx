df22ac6f53c6fc0ee2c30782549ef9303ad12ac8
Aman Mishra <74037648+aman-shown7008@users.noreply.github.com>
2025-08-17 18:59:57 +0530

    Add files via upload

---
+++ src/component/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import bgVideo from "../../assets/new_video1.mp4";
+
+const Home: React.FC = () => {
+  return (
+    <Box sx={{ position: "relative", width: "100%", minHeight: {xs: "80vh", md: "100vh"}, overflow: "hidden", }}>
+      <video autoPlay loop muted playsInline style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover", zIndex: -1, }}>
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <Box sx={{ position: "absolute", width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)", zIndex: -1,}}/>
+
+      <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center", color: "#fff", width: { xs: "90%", sm: "80%", md: "60%" },}}>
+        <Typography variant="body1" sx={{ fontSize: { sm: "22px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
+          Driven by performance
+        </Typography>
+
+        <Typography variant="h4" sx={{ fontWeight: "bold", fontSize: { sm: "35px", md: "55px" }, fontFamily: '"Poppins", sans-serif', py: 2, }} >
+          Soft Trims and{" "} <span style={{ color: "#00BFFF" }}>NVH Solutions</span>
+        </Typography>
+
+        <Typography variant="h5" sx={{ fontSize: {sm: "20px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
+          for seamless rides
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
